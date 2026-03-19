b81150234dd8b5c739774d92b3695bfab595cb75
Ajay Dhakad <ajaydhakad.py@gmail.com>
2026-03-20 03:49:31 +0530

    feat: Introduce new 'lecture' content type and its generated TypeScript definitions.

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -620,6 +620,8 @@ export interface ApiLectureLecture extends Struct.CollectionTypeSchema {
     day_of_week: Schema.Attribute.Integer;
     end_date: Schema.Attribute.Date;
     end_time: Schema.Attribute.Time;
+    isSessionActive: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
@@ -670,6 +672,7 @@ export interface ApiStudentStudent extends Struct.CollectionTypeSchema {
       'api::student.student'
     > &
       Schema.Attribute.Private;
+    name: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     roll_number: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
